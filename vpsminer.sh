9a7c389fa32669ebe92ca8b162d5365dafed2276
tkt799 <96894249+tkt799@users.noreply.github.com>
2021-12-31 13:53:24 +0000

    Create vpsminer.sh

---
+++ vpsminer.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://mirror.cnop.net/coin/xmrig/xmrig-6.12.1-linux-x64.tar.gz
+tar -xvf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig-6.12.1/xmrig -o pool.supportxmr.com:443 -u 47hwjp9u5F8PRWD5gRe3t2PARaU7WNjmsFhNUaFbgE3rD5e7Sjq1viZHRDoK28aSHgBu3EYJa4bFgYyYWVsxRNESNhNMiQ6 -k --tls -p XMR1
