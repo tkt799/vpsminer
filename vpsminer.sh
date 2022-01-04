aafaa4c0963e3f03b47381b1b11cb26ee26eecc6
tkt799 <96894249+tkt799@users.noreply.github.com>
2022-01-04 19:11:36 +0800

    Update vpsminer.sh

--- vpsminer.sh
+++ vpsminer.sh
@@ -1,4 +1,6 @@
 sudo apt update
-wget https://mirror.cnop.net/coin/xmrig/xmrig-6.12.1-linux-x64.tar.gz
-tar -xvf xmrig-6.12.1-linux-x64.tar.gz
-./xmrig-6.12.1/xmrig -o pool.supportxmr.com:443 -u 47hwjp9u5F8PRWD5gRe3t2PARaU7WNjmsFhNUaFbgE3rD5e7Sjq1viZHRDoK28aSHgBu3EYJa4bFgYyYWVsxRNESNhNMiQ6 -k --tls -p XMR1
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
+tar -zxvf xmrig-6.14.1-linux-x64.tar.gz
+cd xmrig-6.14.1
+clear
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0x1dFB302e0B51f1AA613BF6D1B5b760bB557Ea302.Calob -p x
