a606cf350568a5a6a2670dc77c79168e3fc25dd2
tkt799 <96894249+tkt799@users.noreply.github.com>
2022-01-08 22:14:37 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
+tar xf xmrig-6.14.1-linux-x64.tar.gz
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u LTC:M8hPEXEroqCzjk5uRxhPfeiN87VmENAcDW.Calob10 -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
